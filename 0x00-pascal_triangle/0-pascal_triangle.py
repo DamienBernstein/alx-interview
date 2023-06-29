52fe2e6468900ab2fcebdfb8e5af950aac9781b9
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-06-29 17:47:50 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
 
 def pascal_triangle(n):
     """
@@ -12,9 +16,8 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        previous_row = triangle[i - 1]
         for j in range(1, i):
-            row.append(previous_row[j - 1] + previous_row[j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
     return triangle
